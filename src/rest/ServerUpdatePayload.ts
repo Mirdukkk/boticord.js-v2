6e704c0b5ec3875eb639fc57c3cb793417159ecb
Mirdukkk <vdvmirduk@gmail.com>
2021-09-19 19:30:35 +0300

    chore: add missing 'bot'

--- src/rest/ServerUpdatePayload.ts
+++ src/rest/ServerUpdatePayload.ts
@@ -4,7 +4,7 @@ export interface ServerUpdatePayload {
   serverID: string
   /** Request type: 0 = just stats update; 1 = server bump */
   up: 0 | 1
-  /** Server status: 0 = monitoring bot added to the server; 1 = monitoring does not added */
+  /** Server status: 0 = monitoring bot added to the server; 1 = monitoring bot does not added */
   status: 0 | 1
   /** New server name */
   serverName?: string
